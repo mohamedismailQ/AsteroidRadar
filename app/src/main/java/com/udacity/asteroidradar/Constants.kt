2d148ebfe56d76b89133ebfb05d7a6f5ee871448
mohamedismailQ <111974489+mohamedismailQ@users.noreply.github.com>
2022-09-14 15:32:24 +0200

    Update Constants.kt

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "6dHf06W2h7YNkRpgt5rMQdj7bMjgrpx81BmelvUV"
+    const val API_KEY = " put your API here "
     const val ASTEROIDS_URL = "neo/rest/v1/feed"
     const val IMAGE_OF_DAY_URL = "planetary/apod"
 }
